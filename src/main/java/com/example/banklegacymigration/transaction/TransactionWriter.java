b2fa1e1da1449b1147a8b4846c0adc299b545970
Matias Zuniga <mi.zunigac@duocuc.cl>
2026-08-16 23:44:41 -0400

    feat: implementar persistencia con postgresql local

--- src/main/java/com/example/banklegacymigration/transaction/TransactionWriter.java
+++ src/main/java/com/example/banklegacymigration/transaction/TransactionWriter.java
@@ -2,17 +2,36 @@ package com.example.banklegacymigration.transaction;
 
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TransactionWriter
-        implements ItemWriter<Transaction> {
+public class TransactionWriter implements ItemWriter<Transaction> {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public TransactionWriter(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public void write(Chunk<? extends Transaction> transactions) {
 
         for (Transaction transaction : transactions) {
-            System.out.println(transaction);
+
+            jdbcTemplate.update(
+                    """
+                    INSERT INTO transacciones
+                    (id, fecha, monto, tipo, anomalia, motivo)
+                    VALUES (?, ?, ?, ?, ?, ?)
+                    """,
+                    transaction.getId(),
+                    transaction.getFecha(),
+                    transaction.getMonto(),
+                    transaction.getTipo(),
+                    transaction.isAnomalia(),
+                    transaction.getMotivo()
+            );
         }
     }
 }
