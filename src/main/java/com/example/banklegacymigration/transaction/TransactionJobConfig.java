9fe5d40bd69fb24d0a72f569ca65281b6a475973
Matias Zuniga <mi.zunigac@duocuc.cl>
2026-08-17 19:10:45 -0400

    feat: incluir resumen diario desde Job Transaction

--- src/main/java/com/example/banklegacymigration/transaction/TransactionJobConfig.java
+++ src/main/java/com/example/banklegacymigration/transaction/TransactionJobConfig.java
@@ -9,6 +9,7 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class TransactionJobConfig {
@@ -35,10 +36,50 @@ public class TransactionJobConfig {
     @Bean
     public Job transactionJob(
             JobRepository jobRepository,
-            Step transactionStep) {
+            Step transactionStep,
+            Step dailySummaryStep) {
 
         return new JobBuilder("transactionJob", jobRepository)
                 .start(transactionStep)
+                .next(dailySummaryStep)
                 .build();
     }
+
+    @Bean
+    public Step dailySummaryStep(
+        JobRepository jobRepository,
+        PlatformTransactionManager transactionManager,
+        JdbcTemplate jdbcTemplate) {
+
+        return new StepBuilder("dailySummaryStep", jobRepository)
+            .tasklet((contribution, chunkContext) -> {
+
+                jdbcTemplate.update(
+                        """
+                        INSERT INTO resumen_transacciones_diarias (
+                            fecha,
+                            cantidad_transacciones,
+                            monto_total,
+                            cantidad_anomalias
+                        )
+                        SELECT
+                            fecha,
+                            COUNT(*),
+                            COALESCE(SUM(monto), 0),
+                            COUNT(*) FILTER (WHERE anomalia = true)
+                        FROM transacciones
+                        GROUP BY fecha
+
+                        ON CONFLICT (fecha)
+                        DO UPDATE SET
+                            cantidad_transacciones = EXCLUDED.cantidad_transacciones,
+                            monto_total = EXCLUDED.monto_total,
+                            cantidad_anomalias = EXCLUDED.cantidad_anomalias
+                        """
+                );
+
+                return null;
+            }, transactionManager)
+            .build();
+        }
 }
